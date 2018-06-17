35de4f9955abaf056d182e86bd9057258a4a0a1e
Arthur <alteyka.da@gmail.com>
2018-06-17 17:04:45 +0300

    Add files via upload

--- My-Pars/Pars.py
+++ My-Pars/Pars.py
@@ -9,9 +9,9 @@ def get_html(url):
 
 
 def write_csv(data):
-    with open('Викторины.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([data['number'],
+    with open('Викторины.csv', 'a') as f:       # Создаю файл для дозаписи.
+        writer = csv.writer(f)                  # Определяю метод писателя,
+        writer.writerow([data['number'],        # который при вызове запишет данные в csv файл
                          data['question'],
                          data['answer']])
 
@@ -20,9 +20,10 @@ def write_csv(data):
 
 def get_page_data(html):
     soup = BeautifulSoup(html, 'lxml')
-
+# Нахожу нужные теги.
     trs = soup.find_all('tr', {'class': 'tooltip'})
-
+# В каждом trs ищу элементы, записываю в словарь.
+# Записываю словарь в csv с помощь функции выше.
     for tr in trs:
         tds = tr.find_all('td')
         number = tds[0].text
@@ -42,7 +43,7 @@ def get_page_data(html):
 
 def main():
     pattern = 'https://baza-otvetov.ru/categories/view/1/{}'
-
+# В цикле генерирую числа и подставляю каждую итерацию в url
     for i in range(0, 3010, 10):
         url = pattern.format(str(i))
         get_page_data(get_html(url))
